e4fb9328d3c550f468b4a02a0007462c831c6972
carllapierre1 <carl_lapi@hotmail.com>
2021-03-10 12:14:18 -0500

    Small fix

--- commands/talk.js
+++ commands/talk.js
@@ -1,7 +1,7 @@
 const command       = require('../functions/helper-command')
 
 exports.run = async (client, message, args) => {
-    if (message.member.hasPermission("ADMINISTRATOR")){
+    if (message.member.roles.find(role => role.name.toLowerCase() === 'admin')){
         var arg1 = command.getOption(args, 1)
         var arg2 = command.getOption(args, 2)
         var channel = client.channels.find(c => c.name === arg1);
