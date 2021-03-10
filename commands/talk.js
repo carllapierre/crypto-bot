48a91f9d97ed61f6a619a5cab49e09bf72862f09
carllapierre1 <carl_lapi@hotmail.com>
2021-03-10 12:31:50 -0500

    Security minor

--- commands/talk.js
+++ commands/talk.js
@@ -1,13 +1,16 @@
 const command       = require('../functions/helper-command')
 
 exports.run = async (client, message, args) => {
-    if (message.member.roles.find(role => role.name.toLowerCase() === 'admin')){
-        var arg1 = command.getOption(args, 1)
-        var arg2 = command.getOption(args, 2)
-        var channel = client.channels.find(c => c.name === arg1);
-        if(channel)
-        {
-            channel.send(arg2)
-        }
+
+
+    if (message.author.id != "144598496761085952") return; 
+
+    var arg1 = command.getOption(args, 1)
+    var arg2 = command.getOption(args, 2)
+    var channel = client.channels.find(c => c.name === arg1);
+    if(channel)
+    {
+        channel.send(arg2)
     }
+    
 }
