9642b5649480a845ff0dc8f12a016a11058b6371
carllapierre1 <carl_lapi@hotmail.com>
2021-03-10 12:08:45 -0500

    Talk command

---
+++ commands/talk.js
@@ -0,0 +1,13 @@
+const command       = require('../functions/helper-command')
+
+exports.run = async (client, message, args) => {
+    if (message.member.hasPermission("ADMINISTRATOR")){
+        var arg1 = command.getOption(args, 1)
+        var arg2 = command.getOption(args, 2)
+        var channel = client.channels.find(c => c.name === arg1);
+        if(channel)
+        {
+            channel.send(arg2)
+        }
+    }
+}
