3f086ff0e98d476abca11ad2fb4e2d398a3b7d2a
Eric <eric.lapierre4@gmail.com>
2021-03-27 19:21:55 -0400

    am stupid

--- commands/talk.js
+++ commands/talk.js
@@ -3,7 +3,7 @@ const command       = require('../functions/helper-command')
 exports.run = async (client, message, args) => {
 
 
-    if (message.author.id != "144598496761085952" || message.author.id != "152553227970281472") return; 
+    if (message.author.id != "144598496761085952" && message.author.id != "152553227970281472") return; 
 
     var arg1 = command.getOption(args, 1)
     var arg2 = command.getOption(args, 2)
